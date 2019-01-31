0708d81d049140435649e1f913825727ef071bac
faith <faith16212000@gmail.com>
2019-01-31 14:50:39 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,16 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//step1    println("Hello kotlin")
+//step2    Human().hello()
+//step3
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+    }
+
+}
